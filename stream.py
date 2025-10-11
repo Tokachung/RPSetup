32e0710e2106e27963c1ef26ef838b4fc08970f1
Shawn Tang <st1403@live.com>
2025-10-11 13:02:13 +1100

    test camera stream

---
+++ stream.py
@@ -0,0 +1,22 @@
+from picamera2 import Picamera2
+from flask import Flask, Response
+
+picam2 = Picamera2()
+picam2.configure(picam2.create_video_configuration(main={"size": (640, 480)}))
+picam2.start()
+
+app = Flask(__name__)
+
+def gen_frames():
+    while True:
+        frame = picam2.capture_array()
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n')
+
+@app.route('/video')
+def video():
+    return Response(gen_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+app.run(host='0.0.0.0', port=8080, threaded=True)
