f05c8186805bf3efee2a30cc47ae48b0ff8e58e1
Shawn Tang <st1403@live.com>
2025-10-11 14:14:48 +1100

    update auth file

--- stream_with_auth.py
+++ stream_with_auth.py
@@ -1,5 +1,20 @@
 from flask import Flask, Response, request
 from functools import wraps
+from picamera2 import Picamera2
+import cv2
+
+picam2 = Picamera2()
+picam2.configure(picam2.create_video_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+picam2.start()
+
+app = Flask(__name__)
+
+def gen_frames():
+    while True:
+        frame = picam2.capture_array()
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n')
 
 def check_auth(username, password):
     return username == 'user' and password == 'pass'
